6591efbe75b1452e5d715a02ca250716e47e3ee4
lan yang <1356298972@qq.com>
2022-06-07 12:52:39 +0800

    增加car类，设计充电桩类

---
+++ Code/Car.h
@@ -0,0 +1,30 @@
+#include <ctime>
+struct CarAsk //汽车充电请求
+{
+    int UID; // 用户ID
+    int CarID; // 车辆ID
+    int ChargeCap; // 请求充电量
+    bool IsFastCharge; //快充还是慢充，快充为1
+    double BatteryCap; // 电池容量
+    time_t StWaitTime; // 开始等待时间
+};
+struct CarReply //汽车充电响应
+{
+    CarAsk* Ask;
+    int SID; // 充电桩ID
+    int num; // 在该充电桩的排队号
+};
+class Car // 车辆
+{
+private:
+    int UID; // 用户ID
+    int CarID; // 车辆ID
+    int CarName; // 车辆名称
+    double BatteryCap; // 电池容量
+    bool IsAsk; // 是否请求充电，1为是
+    CarAsk* Ask;
+    bool IsReply; // 是否获得reply，1为是
+    CarReply* Reply;
+public:
+
+}
--- Code/ChargePort.h
+++ Code/ChargePort.h
@@ -1,2 +1,18 @@
-class ChargePort{
+#include<vector>
+#include<list>
+#include "Car.h"
+class ChargePort{//充电桩
+private:
+    int SID; // 充电桩编号
+    string type; //快充还是慢充，快充为F,慢充为T
+    std::list<CarReply*>WaitingCar; //当前等待处理的充电响应
+    bool OnState; // 开关状态，1开0关
+    int ChargeCnt; // 累计充电次数
+    double ChargeCost; // 累计充电费用。单位/元
+    double ChargeTime; // 累计充电时长。单位/min
+    double TotalCharge; // 累计总充电量。单位/度
+    double ServiceCost; //累计服务费用。单位/元
+public:
+    StatusTable GetStatus();//返回充电桩状态
+    
 };
