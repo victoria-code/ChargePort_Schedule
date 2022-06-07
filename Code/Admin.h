8253ae0dff40b422a49f03964dd388a293c579e5
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-07 20:29:27 +0800

    Update Admin.h

--- Code/Admin.h
+++ Code/Admin.h
@@ -8,11 +8,11 @@ public:
     //查询充电桩信息(充电桩状态+等候服务车辆信息)
     int getChargePortData();
 
-    //开启充电桩(一次性全部开启)
-    bool openChargePort();
+    //开启充电桩
+    bool openChargePort(int SID);
 
     //关闭充电桩
-    bool closeChargePort();
+    bool closeChargePort(int SID);
 
     //向服务器发送请求
     int sendRequest(string usrname,int cmd,string info);
