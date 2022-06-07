3bed63f63eeecabd297f5c2e77d1492a0e364090
lan yang <1356298972@qq.com>
2022-06-07 12:53:38 +0800

    增加car类，设计充电桩类

--- Code/ChargePort.cpp
+++ Code/ChargePort.cpp
@@ -1,1 +1,7 @@
-
+#include "ChargePort.h"
+const double HIGH_ELEC_PRICE=1.0; //峰电价，单位元/度
+const double LOW_ELEC_PRICE=0.4; //谷电价，单位元/度
+const double NORMAL_ELEC_PRICE=0.7; //平电价，单位元/度
+const double SERVICE_PRICE=0.8; //服务价，单位元/度
+const double FAST_CHARGE_RATE=30; //快充功率，单位度/小时
+const double SLOW_CHARGE_RATE=7; //慢充功率，单位度/小时
