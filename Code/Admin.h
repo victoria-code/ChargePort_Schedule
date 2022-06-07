02a06056383536cd6eace184172a04d7f2de551a
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-07 20:10:49 +0800

    初步定义管理员类(按需修改

--- Code/Admin.h
+++ Code/Admin.h
@@ -1,2 +1,20 @@
-class Admin{
+#include"user.h"
+
+class Admin : public User
+{
+private:
+
+public:
+    //查询充电桩信息(充电桩状态+等候服务车辆信息)
+    int getChargePortData();
+
+    //开启充电桩(一次性全部开启)
+    bool openChargePort();
+
+    //关闭充电桩
+    bool closeChargePort();
+
+    //向服务器发送请求
+    int sendRequest(string usrname,int cmd,string info);
+    
 };
