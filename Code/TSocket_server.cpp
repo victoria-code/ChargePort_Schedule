2665026d69e39e140764e87b9e3208a19ee17aab
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-12 00:17:20 +0800

    Add files via upload

---
+++ Code/TSocket_server.cpp
@@ -0,0 +1,88 @@
+#include "TSocket_server.h"
+
+TSocket server_sock;
+
+ TSocket::TSocket()
+{
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	//创建套接字
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	//绑定套接字
+	memset(&ser_Addr, 0, sizeof(ser_Addr));  //每个字节都用0填充
+	ser_Addr.sin_family = PF_INET;
+	ser_Addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ser_Addr.sin_port = htons(1234);
+	bind(sock, (SOCKADDR*)&ser_Addr, sizeof(SOCKADDR));
+
+	//进入监听状态
+	listen(sock, 20);
+	cout << "监听中..." << endl;
+
+	char bufSend[BUF_SIZE] = { 0 };	// 发送缓冲区
+	char bufRecv[BUF_SIZE] = { 0 };	// 接收缓冲区
+
+	memset(bufSend, 0, BUF_SIZE);  //重置缓冲区
+	memset(bufRecv, 0, BUF_SIZE);  //重置缓冲区
+
+	//接收请求
+	nSize = sizeof(SOCKADDR);
+	clntSock = accept(sock, (SOCKADDR*)&clntAddr, &nSize);
+	if (clntSock != -1)
+		cout << "accept succeed!" << endl;
+	else
+	{
+		cout << "accept error!" << endl;
+	}
+
+}
+
+
+ void TSocket::Send(struct Info& send_info)
+ {
+	 send(clntSock, (char*)&send_info, sizeof(send_info), 0);
+	 memset(&send_info, 0, sizeof(send_info));	//发送后清空结构体
+ }
+
+
+ // 将接收缓冲区复制到msg_recv
+ void TSocket::Recv(struct Info& recv_info)
+ {
+	 memset(bufRecv, 0, BUF_SIZE);	//接收之前重置缓冲区！
+	 memset(&recv_info, 0, sizeof(recv_info));	//清空结构体
+
+	 int strLen = recv(clntSock, bufRecv, BUF_SIZE, 0);
+	 if (strLen <= 0)
+	 {
+		 cout << "Receive from Client Error!" << endl;
+	 }
+
+	 memcpy(&recv_info, bufRecv, sizeof(recv_info));	//拷贝
+
+	 //输出接收到的结构体
+	 cout << "Info type:" << recv_info.info_type << endl
+		 << "Name:" << recv_info.name << endl
+		 << "Password:" << recv_info.password << endl
+		 << "Banlance:" << recv_info.banlance << endl ;
+	 return;
+ }
+
+
+ TSocket::~TSocket()
+{
+
+}
+
+
+ void TSocket::Close()
+ {
+	 server_sock.Send(send_info);
+
+	 closesocket(sock);
+	 closesocket(clntSock);
+	 WSACleanup();
+
+	 cout << endl << "连接已断开..." << endl;
+ }
+
---
+++ Code/TSocket_server.h
@@ -0,0 +1,35 @@
+#include <WinSock2.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include "head.h"
+
+#define BUF_SIZE 200
+
+#pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
+
+class TSocket {
+
+public:
+	WSADATA wsaData;
+	SOCKET sock;			//服务器端套接字
+	sockaddr_in ser_Addr;	//服务器端套接字地址
+
+	SOCKET clntSock;	//客户端套接字
+	SOCKADDR clntAddr;	//客户端套接字地址
+	int nSize;
+	char bufSend[BUF_SIZE];	// 发送缓冲区
+	char bufRecv[BUF_SIZE];	// 接收缓冲区
+
+	TSocket();//构造函数
+	~TSocket();//析构函数
+
+	void Send(struct Info& send_info);//发送结构体
+	void Recv(struct Info& recv_info);//接收结构体
+	void Close();//断开连接
+};
+
+
+extern TSocket server_sock;
+
