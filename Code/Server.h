7f56f29791fba22976971cb06864c37c405d6528
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-12 16:48:36 +0800

    Update Server.h

--- Code/Server.h
+++ Code/Server.h
@@ -3,7 +3,6 @@
 #include "TSocket_server.h"
 #include<algorithm>
 
-//
 extern TSocket server_sock;
 
 //记录用户信息文件名和日志文件名，该文件和服务器端main.cpp位于同一目录下
@@ -17,10 +16,10 @@ class Server{
     public:
 
         /*服务器初始化*/
-        Server::Server();
-        Server::Server(int PID);/**
+        Server();
+        Server(int PID);/**故障情况
         /*服务器下线*/
-        Server::~Server();
+        ~Server();
 
         /*服务器所控制的数据库*/
         DBupdate database;
@@ -33,10 +32,8 @@ class Server{
         int FNum,TNum;  //当前等候区内正在等候的快充/慢充车辆排队号码
         int failID;//出现故障的充电桩ID
         time_t failTime;//出现故障的时间
-        int recvCostTable();//接收充电桩返回的充电详单，同时监测到充电桩空闲后进行调度
         void forwardRequet(string usrname,int SID);//向充电桩转发充电区内充电请求，预备充电
         void sendDetail(ChargeTablePool* next);//向用户发送对应的充电详单并进行扣费
-        int spyChargePort();//充电桩信息监控:获取所有充电桩的相关信息/***
        
        /*记录当前已登录的用户及用户地址*/
         map<string,string>usrAdress;
@@ -46,7 +43,6 @@ class Server{
         map<string,CarAsk*>CUser;    //记录车辆位于充电区的用户及其请求
         map<string,pair<string,int>>queueData;//记录排队号码和前车等待数量
 
-        int recvClient();//接收来自客户端的请求消息
         int replyClient(Info usrInfo);//响应用户客户端请求
         int logIn(string usrname,string passwd,usrEntry* uE);//登录验证，登录成功则获取用户信息
         int signUp(string usrname,string passwd,string role);//注册认证
@@ -56,7 +52,6 @@ class Server{
         int balanceChange(string usrname,int amount);//充值、扣费（customer)
         int copeChargeRequest(CarAsk* ask);//处理等候区内用户的充电请求(customer)
         int cancelCharge(string usrname);//处理用户取消充电申请(customer)
-        void returnBill_schedule();//监测到充电结束后返回充电详情,包含计费信息,检测到有空位是进行调度(customer)
         int getQueueData(string usrname,string &qNum,int &curWait);//获取排队信息(customer)
         string queueNumGenerate(string usrname,int mode);//车辆排队号码生成(customer)
         int getCurWaitNum(string usrname);//获取最新的前车等待数量信息(customer)
@@ -64,7 +59,15 @@ class Server{
         int getFreeCP(int &fSID,int &tSID,int &fTime,int &tTime);//获取充电区内排队队列有空位且等待时间最短的充电桩(customer)
         int resolveRequest(Info &usrInfo,CarAsk *ask);//提取充电请求(customer)
 
+        //ADMIN
+        int openCP(string usrname,int SID);//开启充电桩
+        int closeCP(string usrname,int SID);//关闭充电桩
+        int getChargePortData(string usrname);//获取充电桩状态信息
+        int getReport(string usrname);//获取报表
+        int getCarData(string usrname);//获取等候服务的车辆信息
 
+        //获取时间
+        string getCurTime();
 };
 
 
@@ -72,7 +75,7 @@ class Server{
 class DBupdate{
     public:
         /*数据库初始化*/
-        DBupdate::DBupdate();
+        DBupdate();
 
         /*数据库条目解析*/
         int entryResolve(usrEntry* uE,string line);//用户条目
@@ -86,7 +89,6 @@ class DBupdate{
 
         /*用户信息维护*/
         int addUser(usrEntry *data); //新增用户     
-        int usrDataChange(struct usrEntry* data);//用户信息维护
         
         /*充电日志*/
         int addLogEntry(logEntry *data);//新增日志条目
