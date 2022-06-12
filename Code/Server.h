8da01f69a083f9d5f706bc1eb28206b67543f61c
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-12 11:20:36 +0800

    debug

--- Code/Server.h
+++ Code/Server.h
@@ -1,7 +1,7 @@
 /*服务器端实现*/
-#include "main.h"
 #include "ChargePort.h"
 #include "TSocket_server.h"
+#include<algorithm>
 
 //
 extern TSocket server_sock;
@@ -33,8 +33,9 @@ class Server{
         int FNum,TNum;  //当前等候区内正在等候的快充/慢充车辆排队号码
         int failID;//出现故障的充电桩ID
         time_t failTime;//出现故障的时间
-        void recvCostTable();//收取报表
-        void Server::sendDetail(ChargeTablePool* next);//向用户发送对应的充电详单并进行扣费
+        int recvCostTable();//接收充电桩返回的充电详单，同时监测到充电桩空闲后进行调度
+        void forwardRequet(string usrname,int SID);//向充电桩转发充电区内充电请求，预备充电
+        void sendDetail(ChargeTablePool* next);//向用户发送对应的充电详单并进行扣费
         int spyChargePort();//充电桩信息监控:获取所有充电桩的相关信息/***
        
        /*记录当前已登录的用户及用户地址*/
@@ -46,22 +47,26 @@ class Server{
         map<string,pair<string,int>>queueData;//记录排队号码和前车等待数量
 
         int recvClient();//接收来自客户端的请求消息
-        int replyCLient(Info* usrInfo);//响应用户客户端请求
+        int replyClient(Info usrInfo);//响应用户客户端请求
         int logIn(string usrname,string passwd,usrEntry* uE);//登录验证，登录成功则获取用户信息
         int signUp(string usrname,string passwd,string role);//注册认证
-        int balanceChange(string usrname,int amount);//充值、扣费
         int deleteUsr(string usrname);//用户注销
-        int copeChargeRequest(CarAsk* ask);//处理等候区内用户的充电请求
-        int cancelCharge(string usrname);//处理用户取消充电申请
-        void returnBill_schedule();//监测到充电结束后返回充电详情,包含计费信息,检测到有空位是进行调度
-        int getQueueData(string usrname,string &qNum,int &curWait);//获取排队信息
-        string queueNumGenerate(string usrname,int mode);//车辆排队号码生成
-        int getCurWaitNum(string usrname);//获取最新的前车等待数量信息
-        int Calling(string &fUser,string &tUser);//获取等候区内即将进入充电区的快充和慢充usrname(叫号）
-        int getFreeCP(int &fID,int &tID,int &fTime,int &tTime);//获取充电区内排队队列有空位且等待时间最短的充电桩
 
-        int resolveRequest(Info *usrInfo,CarAsk *ask);//提取充电请求
+        //CUSTOMER
+        int balanceChange(string usrname,int amount);//充值、扣费（customer)
+        int copeChargeRequest(CarAsk* ask);//处理等候区内用户的充电请求(customer)
+        int cancelCharge(string usrname);//处理用户取消充电申请(customer)
+        void returnBill_schedule();//监测到充电结束后返回充电详情,包含计费信息,检测到有空位是进行调度(customer)
+        int getQueueData(string usrname,string &qNum,int &curWait);//获取排队信息(customer)
+        string queueNumGenerate(string usrname,int mode);//车辆排队号码生成(customer)
+        int getCurWaitNum(string usrname);//获取最新的前车等待数量信息(customer)
+        int Calling(string &fUser,string &tUser);//获取等候区内即将进入充电区的快充和慢充usrname(叫号）(customer)
+        int getFreeCP(int &fSID,int &tSID,int &fTime,int &tTime);//获取充电区内排队队列有空位且等待时间最短的充电桩(customer)
+        int resolveRequest(Info &usrInfo,CarAsk *ask);//提取充电请求(customer)
+
+
 };
+
 
 //数据库更新
 class DBupdate{
