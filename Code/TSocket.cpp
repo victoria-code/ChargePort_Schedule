5626a7f7496acc440240c02181622d0b4d764a4f
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-13 08:02:45 +0800

    Update TSocket.cpp

--- Code/TSocket.cpp
+++ Code/TSocket.cpp
@@ -1,17 +1,24 @@
-#include "TSocket.h"
+#include "TSocket_server.h"
 
-//用户端即client_sock  服务器端改为sever_sock即可
-TSocket client_sock;
+TSocket server_sock;
 
-
- TSocket::TSocket()
+TSocket::TSocket()
 {
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	memset(&cli_Addr, 0, sizeof(cli_Addr));  //每个字节都用0填充
-	cli_Addr.sin_family = PF_INET;
-	cli_Addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	cli_Addr.sin_port = htons(1234);
+	//创建套接字
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	//绑定套接字
+	memset(&ser_Addr, 0, sizeof(ser_Addr));  //每个字节都用0填充
+	ser_Addr.sin_family = PF_INET;
+	ser_Addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ser_Addr.sin_port = htons(1234);
+	bind(sock, (SOCKADDR*)&ser_Addr, sizeof(SOCKADDR));
+
+	//进入监听状态
+	listen(sock, 20);
+	cout << "监听中..." << endl;
 
 	char bufSend[BUF_SIZE] = { 0 };	// 发送缓冲区
 	char bufRecv[BUF_SIZE] = { 0 };	// 接收缓冲区
@@ -19,62 +26,58 @@ TSocket client_sock;
 	memset(bufSend, 0, BUF_SIZE);  //重置缓冲区
 	memset(bufRecv, 0, BUF_SIZE);  //重置缓冲区
 
-	//建立连接
-	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (connect(sock, (SOCKADDR*)&cli_Addr, sizeof(SOCKADDR)) == 1)
-		cout << "connect succeed!" << endl;
+	//接收请求
+	nSize = sizeof(SOCKADDR);
+	clntSock = accept(sock, (SOCKADDR*)&clntAddr, &nSize);
+	if (clntSock != -1)
+		cout << "accept succeed!" << endl;
+	else
+	{
+		cout << "accept error!" << endl;
+	}
 
 }
 
 
- void TSocket::Send(struct Info& send_info)
- {
-	 // 将结构体直接转变成字符串输出
-	 send(sock, (char*)&send_info, sizeof(send_info), 0);
-
-	 // 发送后清空结构体
-	 memset(&send_info, 0, sizeof(send_info));	
- }
+void TSocket::Send(struct Info& send_info)
+{
+	send(clntSock, (char*)&send_info, sizeof(send_info), 0);
+	memset(&send_info, 0, sizeof(send_info));	//发送后清空结构体
+}
 
 
- int TSocket::Recv(struct Info& recv_info)
- {
-	 memset(bufRecv, 0, BUF_SIZE);		// 清空缓冲区
-	 memset(&recv_info, 0, sizeof(recv_info));	//清空结构体
+// 将接收缓冲区复制到msg_recv
+void TSocket::Recv(struct Info& recv_info)
+{
+	memset(bufRecv, 0, BUF_SIZE);	//接收之前重置缓冲区！
+	memset(&recv_info, 0, sizeof(recv_info));	//清空结构体
 
-	 int strLen = recv(client_sock.sock, bufRecv, BUF_SIZE, 0);
-	 if (strLen == 0)
-	 {
-		 cout << "Receive from Server Error!" << endl;
-	 }
+	int strLen = recv(clntSock, bufRecv, BUF_SIZE, 0);
+	if (strLen <= 0)
+	{
+		cout << "Receive from Client Error!" << endl;
+	}
 
-	 memcpy(&recv_info, bufRecv, sizeof(recv_info));	//字符串复制到结构体
+	memcpy(&recv_info, bufRecv, sizeof(recv_info));	//拷贝
 
-	 //输出接收到的结构体
-	 //cout << endl << "Info type:" << recv_info.info_type << endl
-		// << "Name:" << recv_info.name << endl
-		// << "Password:" << recv_info.password << endl ;
-
-	 return strLen;
- }
+	return;
+}
 
 
- TSocket::~TSocket()
+TSocket::~TSocket()
 {
 
 }
 
 
- //断开连接
- void TSocket::Close()
- {
-	 send_info.cmd = CLOSE;
-	 client_sock.Send(send_info);
+void TSocket::Close()
+{
+	server_sock.Send(send_info);
 
-	 if (client_sock.Recv(recv_info))
-	 {
-		 closesocket(sock);
-		 WSACleanup();
-	 }
- }
+	closesocket(sock);
+	closesocket(clntSock);
+	WSACleanup();
+
+	cout << endl << "连接已断开..." << endl;
+}
 
