cdca30d307dca622e8e5bb6a57252b496f24eb04
zsy <57083234+victoria-code@users.noreply.github.com>
2022-06-15 12:19:32 +0800

    Update main.h

--- client/main.h
+++ client/main.h
@@ -11,63 +11,63 @@
 #include<iterator>
 #include<mutex>
 
-//�ɱ����
-#define MAX_WAIT_NUM 6  //�Ⱥ������λ����
-#define QUEUE_LENGTH 2  //���׮�ŶӶ��г���
-#define FAST_NUM 2		//�����׮��Ŀ
-#define SLOW_NUM 3		//������׮��Ŀ
+//可变参数
+#define MAX_WAIT_NUM 6  //等候区最大车位容量
+#define QUEUE_LENGTH 2  //充电桩排队队列长度
+#define FAST_NUM 2		//快充充电桩数目
+#define SLOW_NUM 3		//慢充充电桩数目
 
 
-#define FAST_POWER 30   //��书�ʣ���λ����/Сʱ��
-#define SLOW_POWER 7    //���书�ʣ���λ����/Сʱ��
+#define FAST_POWER 30   //快充功率（单位：度/小时）
+#define SLOW_POWER 7    //慢充功率（单位：度/小时）
 
-//���ģʽmode
+//充电模式mode
 #define FAST 1
 #define SLOW 0
 
-//�û��ͻ��˶Է���������cmd��ţ����Լӣ�
-#define LOG_IN  100  //��¼��֤.
-#define SIGN_UP 101  //ע����֤��
-#define Balance_CHANGE 102 //�û���ֵ or �۷ѡ�
-#define DELETE_USER 103//�û�ע����
-#define CHARGE_REQUEST 104  //�ύ�������
-#define CANCEL_REQUEST 105  //ȡ�����
-#define GET_QUEUE_DATA 106  //��ȡ�Ŷ���Ϣ
+//用户客户端对服务器请求cmd编号（可自加）
+#define LOG_IN  100  //登录验证.
+#define SIGN_UP 101  //注册认证·
+#define Balance_CHANGE 102 //用户充值 or 扣费·
+#define DELETE_USER 103//用户注销·
+#define CHARGE_REQUEST 104  //提交充电申请
+#define CANCEL_REQUEST 105  //取消充电
+#define GET_QUEUE_DATA 106  //获取排队信息
 
-//�����������ͻ��˷��͵���Ϣ
-#define DETAIL 300	//����������+����굥+�۷ѳɹ�����
-#define CALL 301	//�кţ��ͻ���������
-#define BREAKDOWN 302 //���׮��������+��ǰ����굥+�۷ѳɹ�����
+//服务端主动向客户端发送的消息
+#define DETAIL 300	//充电完成提醒+充电详单+扣费成功提醒
+#define CALL 301	//叫号，客户进入充电区
+#define BREAKDOWN 302 //充电桩故障提醒+当前充电详单+扣费成功提醒
 
 
-//����Ա�ͻ��˶Է���������cmd��ţ����Լӣ�
-#define SPY_CHARGEPORT  200 //���׮��Ϣ���
-#define WAIT_CAR_DATA 201	//�鿴�Ⱥ�ĳ�����Ϣ
-#define DATA_STATISTICS 202 //�鿴����
-#define OPEN_CHARGEPORT 203 //�򿪳��׮
-#define CLOSE_CHARGEPORT 204//�رճ��׮
+//管理员客户端对服务器请求cmd编号（可自加）
+#define SPY_CHARGEPORT  200 //充电桩信息监控
+#define WAIT_CAR_DATA 201	//查看等候的车辆信息
+#define DATA_STATISTICS 202 //查看报表
+#define OPEN_CHARGEPORT 203 //打开充电桩
+#define CLOSE_CHARGEPORT 204//关闭充电桩
 
-//socket�����õ�������
-#define CLOSE 300 //�Ͽ�����
+//socket连接用的请求编号
+#define CLOSE 300 //断开连接
 
 using namespace std;
 
-//�����Ϣ����
-#define INFO_SIZE 500
+//最大消息长度
+#define INFO_SIZE 1500
 
 struct Info
 {
-	int cmd;	//��Ϣ����
-	char UID[20];	//�û���
-	char PWD[10];	//����	
-	int MODE;		//���ģʽ��0��1��  ���ã��û����� 1Customer  2Admin
-	int Q_NUM;		//�ŶӺ���
-	int W_NUM;		//ǰ���ȴ�����
-	double COST;	//������	���ã���ǰ��ص���
-	double BatteryCap;	//���������
-	double BALANCE;	//���			���ã���������
-	int REPLY;		//��Ӧ����ʹ�� ��ʾ������		���ã����׮���
-	char output[500];//��Ӧ�����ֱ�����
+	int cmd;	//消息类型
+	char UID[20];	//用户名
+	char PWD[10];	//密码	
+	int MODE;		//充电模式：0慢1快  复用：用户类型 1Customer  2Admin
+	int Q_NUM;		//排队号码
+	int W_NUM;		//前车等待数量
+	double COST;	//充电费用	复用：当前电池电量
+	double BatteryCap;	//电池总容量
+	double BALANCE;	//余额			复用：请求充电量
+	int REPLY;		//响应报文使用 表示请求结果		复用：充电桩编号
+	char output[1000];//响应结果，直接输出
 };
 
 extern struct Info send_info, recv_info;
